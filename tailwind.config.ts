859786f9dfe7e5bf9f84c8420e0eb12972958ece
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-26 18:15:19 +0530

    Files generated: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
 	darkMode: ["class"],
@@ -67,6 +68,9 @@ export default {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
+			},
+			fontFamily: {
+				sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
 			},
 			keyframes: {
 				'accordion-down': {
